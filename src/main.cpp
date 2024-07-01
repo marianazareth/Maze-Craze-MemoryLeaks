eab8172c398800edcb793698fc199168f87f929f
marianazareth <88343830+marianazareth@users.noreply.github.com>
2024-06-30 20:49:22 -0600

    Update Main

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,47 @@
 #include <iostream>
 #include "backend.h"
+#include "UI_TitleScreen.h"
 #include "uiMain.h"
 
 int main(int argc, char* argv[]) {
     Backend backend;
-    UI ui;
+    UI_TitleScreen UI_TitleScreen;
+    UI_MAIN UI_Main;
 
-    if (!ui.initialize()) {
+    enum GameState {
+        TITLE_SCREEN, MAIN_PROGRAM, WIN_SCREEN
+    };
+
+    if (!UI_Main.initialize()) {
         return 1;
     }
 
+    GameState gameState = TITLE_SCREEN;
+
+    SDL_Renderer* renderer = UI_Main.getRenderer();
+    SDL_Event event;
     bool running = true;
-    char direction = '\0';
+    char direction = 'x';
 
     while (running) {
-        // running = ui.processInput(direction);
-        if (direction != '\0') {
-            backend.moveCube(direction);
-            direction = '\0';
+        if (gameState == TITLE_SCREEN) {
+            UI_TitleScreen.runTitleScreen(renderer);
+        } else if (gameState == MAIN_PROGRAM) {
+            // UI_Main.runMainProgram(matrix, playerNumber); // AQUI SE UTILIZAN LOS VALORES DEL BACKEND PARA RENDERIZAR
         }
-        ui.renderMainProgram(backend, 1);
-        SDL_Delay(16); // Aproximadamente 60 FPS
+
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+            if (gameState == TITLE_SCREEN) {
+                if (UI_TitleScreen.handleEvents(event)) {
+                    gameState = MAIN_PROGRAM;
+                }
+            } else if (gameState == MAIN_PROGRAM) {
+                // UI_Main.handleEvents(event, gameState);
+            }
+        }
     }
 
     return 0;
